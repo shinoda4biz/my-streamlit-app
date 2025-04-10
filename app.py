982cc9ed8b50a80000dde1b2cfd908fcfe37bb1b
shinoda4biz <shinoda.yoko.4biz@gmail.com>
2025-04-10 21:29:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import random
 
-st.title("こんにちは、Streamlit!")
-st.write("これはGitHubからデプロイされる最初のアプリです。")
+st.title("🎯 おみくじアプリ")
+st.write("ボタンを押して、今日の運勢を占おう！")
 
+if st.button("おみくじを引く"):
+    result = random.choice(["大吉", "中吉", "小吉", "凶", "大凶"])
+    st.subheader(f"🎉 結果：{result} 🎉")
